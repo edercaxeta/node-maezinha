e01a9ec302d3e53384614ce130f8a58b08b8c0c9
manoel neto <manoelriosneto@gmail.com>
2022-09-15 22:39:21 -0300

    adicionando página de home

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,39 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { StatusBar} from 'react-native'
+import { 
+  useFonts,
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_900Black
+} from '@expo-google-fonts/inter';
 
-interface ButtonProps {
-  title: string;  
-}
-
-function Button(props: ButtonProps){
-  return(
-    <TouchableOpacity>
-      <Text>{props.title}</Text>
-    </TouchableOpacity>
-  )
-
-}
+import { Home } from './src/screens/Home';
+import { Loading } from './src/components/Loading';
+import { Background } from './src/components/Background';
 
 export default function App() {
+
+  const [fontsLoaded] = useFonts ({    
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_900Black
+  });
+
   return (
-    <View style={styles.container}>
-      <Text> Hello React Native</Text>            
+    <Background>
+      <StatusBar 
+        barStyle="light-content"
+        backgroundColor ="transparent"
+        translucent
+      />      
 
-      <Button title='Entrar'/>
-
-      <StatusBar style="auto" />
-    </View>
+      { fontsLoaded ? <Home/> : <Loading />}
+      
+              
+    </Background>
   );
 }
 
 
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-
-});
