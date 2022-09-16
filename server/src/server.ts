a993d43b7121295ae27fe6a776d8125f9c9290ae
manoel neto <manoelriosneto@gmail.com>
2022-09-16 13:53:52 -0300

    Adicionando rotas de anúncios e games e criando banco de dados

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,8 +2,24 @@ import express from 'express';
 
 const app = express();
 
-app.get('/', (request, response) => {
-  return response.json([{id: "Manoel"}])
+app.get('/games', (request, response) =>  {
+  return response.json([]);
+});
+
+app.post('/ads', (request, response) =>  {
+  return response.status(201).json([]);
+});
+
+app.get('/games/:id/ads', (request, response) => {    
+  /* const gameId = request.params.id; */
+  return response.json([
+    {id: 1, name: 'Anúncio 1'}
+  ]);
+})
+
+app.get('/ads/:id/discord', (request, response) => {    
+  /* const adId = request.params.id; */
+  return response.json([ ])
 })
 
 app.listen(3333, () => console.log("Rodando na porta 3333..."))
