73d3ca672ffbe7f0472da09945e6408b38ca2880
manoel neto <manoelriosneto@gmail.com>
2022-09-18 12:24:16 -0300

    Correção do cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import cors from 'express';
+import cors from 'cors';
 
 import { PrismaClient } from '@prisma/client';
 import { convertHourStringToMinutes } from './utils/convert-hour-string-to-minutes';
@@ -8,7 +8,7 @@ import { convertMinutesToHourString } from './utils/convert-minutes-to-hour-stri
 const app = express();
 
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 /* Para delimitar o uso da api apenas para o domínio próprio:
 origin: 'https://meudominio.com.br' */
 
